20108da1c6977e2b4f3f301a4fe181a25fbca0e3
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-05-24 21:55:58 +0530

    Added the framework picker in the plugin

--- lua/platformio/pioinitialise.lua
+++ lua/platformio/pioinitialise.lua
@@ -10,6 +10,26 @@ local board_data = {}
 local board_names = {}
 local selected_board_id, selected_board_name, selected_board_framework
 
+
+function pick_framework()
+    pickers.new(opts, {
+        prompt_title = "frameworks",
+        finder = finders.new_table{
+            results = board_data[selected_board_name]['frameworks'],
+        },
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            selected_board_framework = selection[1]
+            print(selected_board_name, selected_board_id, selected_board_framework)
+          end)
+          return true
+        end,
+        sorter = conf.generic_sorter(opts),
+    }):find()
+end
+
 local pick_board = function()
     local opts = opts or {}
     pickers.new(opts, {
@@ -23,6 +43,7 @@ local pick_board = function()
             local selection = action_state.get_selected_entry()
             selected_board_name = selection[1]
             selected_board_id = board_data[selection[1]]['id']
+            pick_framework()
           end)
           return true
         end,

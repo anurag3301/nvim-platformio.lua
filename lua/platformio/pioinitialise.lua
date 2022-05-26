b5173669d6b4af68891a192b200eb783b3859144
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-05-26 22:42:26 +0530

    Added integration of specify framework for the board

--- lua/platformio/pioinitialise.lua
+++ lua/platformio/pioinitialise.lua
@@ -23,7 +23,7 @@ function pick_framework()
             local selection = action_state.get_selected_entry()
             selected_board_framework = selection[1]
             print(selected_board_name, selected_board_id, selected_board_framework)
-            vim.cmd("2TermExec cmd='pio project init --board ".. selected_board_id .. "; " .. extra .. "' direction=float")
+            vim.cmd("2TermExec cmd='pio project init --board ".. selected_board_id .. " --project-option=\"framework=" .. selected_board_framework .. "\"; " .. extra .. "' direction=float")
           end)
           return true
         end,

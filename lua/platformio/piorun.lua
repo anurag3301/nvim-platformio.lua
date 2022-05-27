d54fe6e6bcef768cb6d4349d03d1978f97244cb7
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-05-27 12:04:57 +0530

    Added function for piorun and added the respective vim command

--- lua/platformio/piorun.lua
+++ lua/platformio/piorun.lua
@@ -5,6 +5,14 @@ local function file_exists(name)
    if f~=nil then io.close(f) return true else return false end
 end
 
+local function strsplit (inputstr)
+    local t={}
+    for str in string.gmatch(inputstr, "([^%s]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
 local paths = {'.', '..', '../..', '../../..'}
 local inipath = ''
 local extra = 'echo \"\\\\n\\\\033[0;33mPlease Press ENTER to continue \\\\033[0m\"; read; exit;'
@@ -28,6 +36,22 @@ end
 function M.pioupload()
     cd_pioini()
     vim.cmd("2TermExec cmd='pio run --target upload; " .. extra .. "' direction=float")
+end
+
+function M.piorun(arg)
+    if(arg == nil)then
+        arg = 'upload'
+    end
+
+    arg = strsplit(arg)[1]
+    if(arg == 'upload')then
+        M.pioupload()
+    elseif(arg == 'build')then
+        M.piobuild()
+    else
+        print("Invalid argument: upload or build")
+    end
+
 end
 
 return M
--- plugin/platformio.vim
+++ plugin/platformio.vim
@@ -1,1 +1,2 @@
 command Pioinit lua require('platformio.pioinitialise').pioinit()
+command -nargs=* Piorun lua require('platformio.piorun').piorun(<f-args>)

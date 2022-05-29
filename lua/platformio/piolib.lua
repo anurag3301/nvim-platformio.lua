8aa93c711da09fc2342d66496a031c12b3874d85
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-05-29 18:21:24 +0530

    Give library name as arg list

--- lua/platformio/piolib.lua
+++ lua/platformio/piolib.lua
@@ -3,13 +3,18 @@ local M = {}
 local http = require("socket.http")
 local ltn12 = require("ltn12")
 
-function M.piolib()
+function M.piolib(lib_arg_list)
     json_str = ""
+    lib_str = ""
     local lib_name = {}
     local lib_data = {}
     local resp= {}
 
-    url = 'https://api.registry.platformio.org/v3/search?query=%22ArduinoJson%22&page=1&limit=50'
+    for k,v in pairs(lib_arg_list) do
+        lib_str = lib_str .. v 
+    end
+
+    url = 'https://api.registry.platformio.org/v3/search?query=%22' .. lib_str .. '%22&page=1&limit=50'
 
     client, code, headers, status = http.request{url=url, sink=ltn12.sink.table(resp), method="GET"}
 
@@ -31,6 +36,6 @@ function M.piolib()
     end
 end
 
-M.piolib()
+M.piolib({'Arduino', 'Json'})
 
 return M

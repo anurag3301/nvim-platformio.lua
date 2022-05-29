d3db41f9743742a0784dc91e9acafa3ef5d4b8d2
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-05-29 18:04:13 +0530

    Formated the code for requst and parse

--- lua/platformio/piolib.lua
+++ lua/platformio/piolib.lua
@@ -1,29 +1,36 @@
-http = require("socket.http")
-ltn12 = require("ltn12")
+local M = {}
 
-resp, r = {}, {}
+local http = require("socket.http")
+local ltn12 = require("ltn12")
 
-url = 'https://api.registry.platformio.org/v3/search?query=%22ArduinoJson%22&page=1&limit=50'
-args = {}
-
-client, code, headers, status = http.request{url=url, sink=ltn12.sink.table(resp),
-                                            method="GET"}
-
-
-if (code == 200) then
+function M.piolib()
     json_str = ""
-    for k,v in pairs(resp) do
-        json_str = json_str .. v
-    end
-    local json_data = require('lunajson').decode(json_str)
     local lib_name = {}
     local lib_data = {}
+    local resp= {}
 
-    for k,v in pairs(json_data['items']) do
-        lib_data[v['name']] = v
-        table.insert(lib_name, v['name'])
+    url = 'https://api.registry.platformio.org/v3/search?query=%22ArduinoJson%22&page=1&limit=50'
+
+    client, code, headers, status = http.request{url=url, sink=ltn12.sink.table(resp), method="GET"}
+
+    if (code == 200) then
+
+        for k,v in pairs(resp) do
+            json_str = json_str .. v
+        end
+
+        local json_data = require('lunajson').decode(json_str)
+
+        for k,v in pairs(json_data['items']) do
+            lib_data[v['name']] = v
+            table.insert(lib_name, v['name'])
+        end
+
+        -- print(vim.inspect(lib_data))
+        print(vim.inspect(lib_name))
     end
-
-    print(vim.inspect(lib_data))
 end
 
+M.piolib()
+
+return M

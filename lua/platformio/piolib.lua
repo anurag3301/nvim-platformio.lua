25484429b3ef4126840f2266c773f65c56812ecc
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-01-23 19:59:32 +0530

    Removed http.socket dependency, using curl from plenary

--- lua/platformio/piolib.lua
+++ lua/platformio/piolib.lua
@@ -1,7 +1,6 @@
 local M = {}
 
-local http = require("socket.http")
-local ltn12 = require("ltn12")
+local curl = require("plenary.curl")
 
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
@@ -37,27 +36,20 @@ end
 function M.piolib(lib_arg_list)
     if not utils.pio_install_check() then return end
 
-    local json_str = ""
     local lib_str = ""
     local lib_name = {}
     local lib_data = {}
-    local resp= {}
 
     for k,v in pairs(lib_arg_list) do
-        lib_str = lib_str .. v 
+        lib_str = lib_str .. v
     end
 
-    local url = 'https://api.registry.platformio.org/v3/search?query=%22' .. lib_str .. '%22&page=1&limit=50'
+    local url = 'https://api.registry.platformio.org/v3/search?query="' .. lib_str .. '"&page=1&limit=50'
 
-    local client, code, headers, status = http.request{url=url, sink=ltn12.sink.table(resp), method="GET"}
+    local res = curl.get(url, {accept = "application/json"})
 
-    if (code == 200) then
-
-        for k,v in pairs(resp) do
-            json_str = json_str .. v
-        end
-
-        local json_data = vim.json.decode(json_str)
+    if (res['status'] == 200) then
+        local json_data = vim.json.decode(res['body'])
 
         for k,v in pairs(json_data['items']) do
             lib_data[v['name']] = v

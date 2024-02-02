afb950ae9a63b87d270405aaed8115721c69f437
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-02 21:16:34 +0530

    Integrated the new telescope library picker

--- lua/platformio/piolib.lua
+++ lua/platformio/piolib.lua
@@ -4,30 +4,67 @@ local curl = require("plenary.curl")
 
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
+local entry_display = require "telescope.pickers.entry_display"
+local make_entry = require "telescope.make_entry"
 local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 local utils = require('platformio.utils')
 local Terminal  = require('toggleterm.terminal').Terminal
 
-local function pick_library(args)
+local libentry_maker = function(opts)
+  local displayer = entry_display.create {
+    separator = "▏",
+    items = {
+      { width = 20 },
+      { width = 20 },
+      { remaining = true },
+    },
+  }
+
+  local make_display = function(entry)
+    return displayer {
+      entry.value.name,
+      entry.value.owner,
+      entry.value.description,
+    }
+  end
+
+  return function(entry)
+    return make_entry.set_default_entry_mt({
+      value = {
+        name = entry.name,
+        owner = entry.owner.username,
+        description = entry.description,
+      },
+      ordinal = entry.name .. " " .. entry.owner.username .. " " .. entry.description,
+      display = make_display,
+    }, opts)
+  end
+end
+
+local function pick_library(json_data)
     local opts = {}
+    table.sort(json_data['items'], function(lhs, rhs)
+        return lhs.popularity_rank < rhs.popularity_rank
+    end)
     pickers.new(opts, {
         prompt_title = "Libraries",
         finder = finders.new_table{
-            results = args.lib_name,
+            results = json_data['items'],
+            entry_maker = opts.entry_maker or libentry_maker(opts),
         },
         attach_mappings = function(prompt_bufnr, map)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-            selected_library = selection[1]
-            local pkg_name = args['lib_data'][selected_library]['owner']['username'] .. "/" .. selected_library
-            local command = "pio pkg install --library \"".. pkg_name .. "\"; " .. utils.extra
-            local term = Terminal:new({cmd = command, direction = "float"})
-            term:toggle()
-          end)
-          return true
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                local selection = action_state.get_selected_entry()
+                local pkg_name = selection['value']['owner'] .. "/" .. selection['value']['name']
+                local command = "pio pkg install --library \"".. pkg_name .. "\"; " .. utils.extra
+                local term = Terminal:new({cmd = command, direction = "float"})
+                term:toggle()
+            end)
+            return true
         end,
         sorter = conf.generic_sorter(opts),
     }):find()
@@ -51,12 +88,7 @@ function M.piolib(lib_arg_list)
     if (res['status'] == 200) then
         local json_data = vim.json.decode(res['body'])
 
-        for k,v in pairs(json_data['items']) do
-            lib_data[v['name']] = v
-            table.insert(lib_name, v['name'])
-        end
-
-        pick_library({['lib_name']=lib_name, ['lib_data']=lib_data})
+        pick_library(json_data)
     else
         vim.notify("API Request to platformio return HTTP code: " .. 
         code .. "\nplease run `curl -LI " .. url .. "` for complete information", 

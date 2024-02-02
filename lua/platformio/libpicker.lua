7be87287c68d25cfd673e5641544d2889506a5ec
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-02 20:32:40 +0530

    Work on new telescope lib picker

---
+++ lua/platformio/libpicker.lua
@@ -0,0 +1,10 @@
+local function read_file(path)
+    local file = io.open(path, "rb") -- r read mode and b binary mode
+    if not file then return nil end
+    local content = file:read "*a" -- *a or *all reads the whole file
+    file:close()
+    return content
+end
+
+local fileContent = read_file("data.json");
+print (fileContent);

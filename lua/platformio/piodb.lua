4bd854ccc3ed88466e076124d7395df826eac75e
batoaqaa <ahmed.batoaq@gmail.com>
2024-08-23 07:22:49 +0300

    Include Windows support)

---
+++ lua/platformio/piodb.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+local utils = require("platformio.utils")
+
+function M.piodb()
+  local command = "pio run -t compiledb" .. utils.extra
+  utils.ToggleTerminal(command, "float")
+end
+
+return M

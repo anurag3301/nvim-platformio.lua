cd92af60176679541b3d82cf3129a1eba24cd20a
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2024-11-12 23:51:15 +0700

    Adding non-args commands of Pioinit and Piodb

--- plugin/platformio.lua
+++ plugin/platformio.lua
@@ -1,9 +1,22 @@
+-- Pioinit
+vim.api.nvim_create_user_command(
+  "Pioinit",
+  function()
+    require("platformio.pioinit").pioinit()
+  end,
+  {}
+)
+
+-- Piodb
+-- vim.api.nvim_create_user_command("Piodb", require("platformio.piodb").piodb(), {})
+
+-- Piorun
 vim.api.nvim_create_user_command("Piorun", function(opts)
 	local args = vim.split(opts.args, " ")
-	if vim.tbl_contains({ "upload", "build", "clean" }, args[1]) then
+	if vim.tbl_contains({ "upload", "build", "clean" }, args[1]) then -- check valid commands
 		require("platformio.piorun").piorun(args)
 	else
-		vim.api.nvim_err_writeln("Invalid argument. Use 'upload', 'build', or 'clean'.")
+		vim.api.nvim_err_writeln("Invalid argument. Use 'upload', 'build', or 'clean'.") -- error message if the cmd is invalid
 	end
 end, {
 	nargs = 1, -- Only one argument is expected
@@ -11,3 +24,5 @@ end, {
 		return { "upload", "build", "clean" } -- Autocompletion options
 	end,
 })
+
+-- vim.api.nvim_create_user_command("MyFirstFunction", require("plugin_name").hello, {})

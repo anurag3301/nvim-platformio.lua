207b67d4eab36e73b6c0abac1e5b70e980ee08d9
batoaqaa <ahmed.batoaq@gmail.com>
2024-11-18 00:18:36 +0300

    to support: pio run --target uploadfs

--- lua/platformio/piorun.lua
+++ lua/platformio/piorun.lua
@@ -1,23 +1,29 @@
 local M = {}
 
-local utils = require("platformio.utils")
+local utils = require 'platformio.utils'
 
 function M.piobuild()
   utils.cd_pioini()
-  local command = "pio run" .. utils.extra
-  utils.ToggleTerminal(command, "float")
+  local command = 'pio run' .. utils.extra
+  utils.ToggleTerminal(command, 'float')
 end
 
 function M.pioupload()
   utils.cd_pioini()
-  local command = "pio run --target upload" .. utils.extra
-  utils.ToggleTerminal(command, "float")
+  local command = 'pio run --target upload' .. utils.extra
+  utils.ToggleTerminal(command, 'float')
+end
+
+function M.piouploadfs()
+  utils.cd_pioini()
+  local command = 'pio run --target uploadfs' .. utils.extra
+  utils.ToggleTerminal(command, 'float')
 end
 
 function M.pioclean()
   utils.cd_pioini()
-  local command = "pio run --target clean" .. utils.extra
-  utils.ToggleTerminal(command, "float")
+  local command = 'pio run --target clean' .. utils.extra
+  utils.ToggleTerminal(command, 'float')
 end
 
 function M.piorun(arg)
@@ -26,18 +32,20 @@ function M.piorun(arg)
   end
 
   if arg == nil then
-    arg = "upload"
+    arg = 'upload'
   end
 
-  arg = utils.strsplit(arg, "%s")[1]
-  if arg == "upload" then
+  arg = utils.strsplit(arg, '%s')[1]
+  if arg == 'upload' then
     M.pioupload()
-  elseif arg == "build" then
+  elseif arg == 'uploadfs' then
+    M.piouploadfs()
+  elseif arg == 'build' then
     M.piobuild()
-  elseif arg == "clean" then
+  elseif arg == 'clean' then
     M.pioclean()
   else
-    vim.notify("Invalid argument: build, upload or clean", vim.log.levels.WARN)
+    vim.notify('Invalid argument: build, upload or clean', vim.log.levels.WARN)
   end
 end
 
--- plugin/platformio.lua
+++ plugin/platformio.lua
@@ -1,72 +1,74 @@
 -- Pioinit
-vim.api.nvim_create_user_command("Pioinit", function()
-	require("platformio.pioinit").pioinit()
+vim.api.nvim_create_user_command('Pioinit', function()
+  require('platformio.pioinit').pioinit()
 end, {})
 
 -- Piodb
-vim.api.nvim_create_user_command("Piodb", function()
-	require("platformio.piodb").piodb()
+vim.api.nvim_create_user_command('Piodb', function()
+  require('platformio.piodb').piodb()
 end, {})
 
 -- Piorun
-vim.api.nvim_create_user_command("Piorun", function(opts)
-	local args = opts.args
-	if args == "upload" then -- checking valid commands
-		require("platformio.piorun").pioupload()
-	elseif args == "build" then
-		require("platformio.piorun").piobuild()
-	elseif args == "clean" then
-		require("platformio.piorun").pioclean()
-	else
-		vim.api.nvim_err_writeln("Invalid argument. Use 'upload', 'build', or 'clean'.") -- error message if the cmd is invalid
-	end
+vim.api.nvim_create_user_command('Piorun', function(opts)
+  local args = opts.args
+  if args == 'upload' then -- checking valid commands
+    require('platformio.piorun').pioupload()
+  elseif args == 'uploadfs' then
+    require('platformio.piorun').piouploadfs()
+  elseif args == 'build' then
+    require('platformio.piorun').piobuild()
+  elseif args == 'clean' then
+    require('platformio.piorun').pioclean()
+  else
+    vim.api.nvim_err_writeln "Invalid argument. Use 'upload', 'build', or 'clean'." -- error message if the cmd is invalid
+  end
 end, {
-	nargs = 1, -- Only one argument is expected
-	complete = function(_, _, _)
-		return { "upload", "build", "clean" } -- Autocompletion options
-	end,
+  nargs = 1, -- Only one argument is expected
+  complete = function(_, _, _)
+    return { 'upload', 'build', 'clean' } -- Autocompletion options
+  end,
 })
 
 -- Piomon
-vim.api.nvim_create_user_command("Piomon", function(opts)
-	local args = opts.args
-	if args == "" then
-		-- No argument provided, run without args
-		require("platformio.piomon").piomon({})
-	else
-		-- One argument provided, pass it as a table
-		require("platformio.piomon").piomon({ args })
-	end
+vim.api.nvim_create_user_command('Piomon', function(opts)
+  local args = opts.args
+  if args == '' then
+    -- No argument provided, run without args
+    require('platformio.piomon').piomon {}
+  else
+    -- One argument provided, pass it as a table
+    require('platformio.piomon').piomon { args }
+  end
 end, {
-	nargs = "?", -- Allow zero or one argument
-	complete = function(_, _, _)
-		return { "4800", "9600", "57600", "115200" }
-	end,
+  nargs = '?', -- Allow zero or one argument
+  complete = function(_, _, _)
+    return { '4800', '9600', '57600', '115200' }
+  end,
 })
 
 -- Piolib
-vim.api.nvim_create_user_command("Piolib", function(opts)
-	-- Split the args into a table
-	local args = vim.split(opts.args, " ")
+vim.api.nvim_create_user_command('Piolib', function(opts)
+  -- Split the args into a table
+  local args = vim.split(opts.args, ' ')
 
-	-- Call the piolib function with the arguments
-	require("platformio.piolib").piolib(args)
+  -- Call the piolib function with the arguments
+  require('platformio.piolib').piolib(args)
 end, {
-	nargs = "+", -- means can take another argument
+  nargs = '+', -- means can take another argument
 })
 
 -- Piocmd
-vim.api.nvim_create_user_command("Piocmd", function(opts)
-	-- Split the args into a table
-	local cmd_table = vim.split(opts.args, " ")
+vim.api.nvim_create_user_command('Piocmd', function(opts)
+  -- Split the args into a table
+  local cmd_table = vim.split(opts.args, ' ')
 
-	-- Call the piolib function with the arguments
-	require("platformio.pioterm").piocmd(cmd_table)
+  -- Call the piolib function with the arguments
+  require('platformio.pioterm').piocmd(cmd_table)
 end, {
-	nargs = "+", -- means can take another argument
+  nargs = '+', -- means can take another argument
 })
 
 -- Piodebug
-vim.api.nvim_create_user_command("Piodebug", function()
-	require("platformio.piodb").piodb()
+vim.api.nvim_create_user_command('Piodebug', function()
+  require('platformio.piodb').piodb()
 end, {})

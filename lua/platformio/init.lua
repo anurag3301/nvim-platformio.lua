ae27a5b3da821dab13c8b30e8ca0d4da7da69a4a
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2025-07-08 12:07:12 +0530

    Added configurability to piomenu

--- lua/platformio/init.lua
+++ lua/platformio/init.lua
@@ -3,6 +3,49 @@ local M = {}
 local default_config = {
   lsp = 'ccls',
   menu_key = nil,
+
+  menu_bindings = {
+    { group = '  [g]eneral', key = 'g', elements = {
+      { key = 'b', cmd = 'run', desc = ' [b]uild' },
+      { key = 'c', cmd = 'run -t clean', desc = ' [c]lean' },
+      { key = 'f', cmd = 'run -t fullclean', desc = ' [f]ull clean' },
+      { key = 'd', cmd = 'device list', desc = ' [d]evice list' },
+      { key = 'm', cmd = 'run -t monitor', desc = ' [m]onitor' },
+      { key = 'u', cmd = 'run -t upload', desc = ' [u]pload' },
+      { key = 's', cmd = 'run -t uploadfs', desc = ' upload file [s]ystem' },
+      { key = 't', cmd = '', desc = ' Core CLI [T]erminal' },
+    }},
+    { group = '  [d]ependencies', key = 'd', elements = {
+      { key = 'l', cmd = 'pkg list', desc = ' [l]ist packages' },
+      { key = 'o', cmd = 'pkg outdated', desc = ' List [o]utdated packages' },
+      { key = 'u', cmd = 'pkg update', desc = ' [u]pdate packages' },
+    }},
+    { group = '  [a]dvance', key = 'a', elements = {
+      { key = 't', cmd = 'test', desc = ' [t]est' },
+      { key = 'c', cmd = 'check', desc = ' [c]heck' },
+      { key = 'd', cmd = 'debug', desc = ' [d]ebug' },
+      { key = 'b', cmd = 'run -t compiledb', desc = ' compilation data[b]ase' },
+    }},
+    { group = '  [v]erbose', key = 'av', elements = {
+      { key = 'v', cmd = 'debug', desc = ' [v]erbose' },
+      { key = 'b', cmd = 'run -v', desc = ' [b]uild' },
+      { key = 'd', cmd = 'debug -v', desc = ' [d]ebug' },
+      { key = 'u', cmd = 'run -v -t upload', desc = ' [u]pload' },
+      { key = 's', cmd = 'run -v -t uploadfs', desc = ' upload file [s]ystem' },
+      { key = 't', cmd = 'test -v', desc = ' [t]est' },
+      { key = 'c', cmd = 'check -v', desc = ' [c]heck' },
+      { key = 'a', cmd = 'run -v -t compiledb', desc = ' compilation databa[a]e' },
+    }},
+    { group = '  [r]emote', key = 'r', elements = {
+      { key = 'u', cmd = 'remote run -t upload', desc = ' [u]pload' },
+      { key = 't', cmd = 'remote test', desc = ' [t]est' },
+      { key = 'm', cmd = 'remote run -t monitor', desc = ' [m]onitor' },
+      { key = 'd', cmd = 'remote device list', desc = ' [d]evice list' },
+    }},
+    { group = '  [m]iscellaneous', key = 'm', elements = {
+      { key = 'u', cmd = 'upgrade', desc = ' [u]pgrade' },
+    }},
+  }
 }
 
 M.config = vim.deepcopy(default_config)
@@ -11,6 +54,7 @@ function M.setup(user_config)
   local valid_keys = {
     lsp = true,
     menu_key = true,
+    menu_bindings = true,
   }
   for key, _ in pairs(user_config or {}) do
     if not valid_keys[key] then

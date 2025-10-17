21a6f5079c33b1edafbb11499cbaf29be9c96734
Daffa Haj Tsaqif <narutohaj00@gmail.com>
2025-10-17 21:14:54 +0700

    feat(add checks for piodb like piodebug):
    This code seems lacking in safety checks similar to piodebug, either
    it's not visited before or what

--- lua/platformio/piodb.lua
+++ lua/platformio/piodb.lua
@@ -1,8 +1,13 @@
+local utils = require('platformio.utils')
 local M = {}
 
-local utils = require('platformio.utils')
-
 function M.piodb()
+  if not utils.pio_install_check() then
+    return
+  end
+
+  utils.cd_pioini()
+
   local command = 'pio run -t compiledb' -- .. utils.extra
   utils.ToggleTerminal(command, 'float')
 end

a90af642e021d77cd07523b811d4014d88e2ee7f
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2025-10-21 12:24:06 +0530

    show message to restart lsp after PioLSP is ran

--- lua/platformio/piolsp.lua
+++ lua/platformio/piolsp.lua
@@ -4,11 +4,12 @@ local utils = require('platformio.utils')
 local config = require('platformio').config
 
 function M.piolsp()
+  local lsp_message = ' && echo -e "\\n\\n\\033[1;33mPlease run :LspRestart to make changes effective.\\033[0m\\n\\n"'
   if config.lsp == 'clangd' then 
-    local command = 'pio run -t compiledb'
+    local command = 'pio run -t compiledb' .. lsp_message
     utils.ToggleTerminal(command, 'float')
   elseif config.lsp == 'ccls' then
-    local command = 'pio project init --ide=vim'
+    local command = 'pio project init --ide=vim' .. lsp_message
     utils.ToggleTerminal(command, 'float')
   else
     vim.notify('No valid LSP selected!', vim.log.levels.WARN)

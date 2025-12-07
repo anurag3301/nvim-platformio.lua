35c59debb016e613f8c6594866e553ff01e8c348
Anurag Kumar Singh <52702259+anurag3301@users.noreply.github.com>
2025-12-07 20:54:02 +0530

    Added missing confing in minimal_config

--- minimal_config.lua
+++ minimal_config.lua
@@ -72,8 +72,10 @@ require('lazy').setup(plugins, {
 vim.opt['number'] = true
 
 vim.g.pioConfig ={
-    lsp = 'clangd',
-    menu_key = '<leader>\\', -- replace this menu key  to your convenience
+  lsp = 'clangd',           -- value: clangd | ccls 
+  clangd_source = 'ccls',    -- value: ccls | compiledb, For detailed explation check :help platformio-clangd_source
+  menu_key = '<leader>\\',  -- replace this menu key  to your convenience
+  debug = false             -- enable debug messages
 } 
 local pok, platformio = pcall(require, 'platformio')
 if pok then platformio.setup(vim.g.pioConfig) end

7604a4dbe35945dcf8e34bebb073fd3710c152be
Shoxx98 <jonas.g.jobs@gmail.com>
2026-06-05 14:24:56 +0200

    now testing how to do ui_select

--- lua/platformio/pioenv.lua
+++ lua/platformio/pioenv.lua
@@ -1,4 +1,5 @@
 local utils = require('platformio.utils')
+local picker = require('platformio.pickers')
 local M = {}
 
 function M.pioenv()
@@ -36,7 +37,7 @@ function M.pioenv()
     -- _ = stdio_stream.read(stdio_stream, '*l')
 
     --- @type string[]
-    local table_lines = {}
+    local envs = {}
     --- @type integer | nil
     local env_name_end
     _ = stdio_stream.read(stdio_stream, '*line')
@@ -45,7 +46,7 @@ function M.pioenv()
         env_name_end, _ = string.find(line, ' ', 2) -- if cmd succeeds, the env name has to be at least 1 char long and contains no spaces. skip the first one and search for space
         env_name_end = env_name_end - 1
         if env_name_end then -- would start at 1, regardless of start index, so we don't need to differentiate between 0 and nil
-            table.insert(table_lines, string.sub(line, 1, env_name_end))
+            table.insert(envs, string.sub(line, 1, env_name_end))
         else
             vim.notify('formatting pio output failed on finding the first space char', vim.log.levels.ERROR)
             io.close(stdio_stream)
@@ -56,6 +57,9 @@ function M.pioenv()
 
     io.close(stdio_stream)
     -- TODO: execute picker
+    picker.pick_env(envs, function(selected_env)
+        vim.notify(selected_env, loglevel)
+    end)
 end
 
 return M

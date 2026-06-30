fafb7599732500598492beb4ce728c50f8f9b1c2
Shoxx98 <jonas.g.jobs@gmail.com>
2026-06-30 11:06:01 +0200

    now automatically replaces a symlink to compile db in project root and is faster on linux

--- lua/platformio/pioenv.lua
+++ lua/platformio/pioenv.lua
@@ -3,17 +3,52 @@ local picker = require('platformio.pickers')
 local M = {
   default_env = 'none',
 }
+local config = require('platformio').config
+
+--- changes symlink if feasible.
+function M.change_compiledb_symlink(env)
+  local pathsep = '/'
+  if utils.is_windows then
+    pathsep = '\\'
+  end
+  if M.default_env == 'none' then
+    vim.notify('compiledb does not exist for env ' .. M.default_env, vim.log.levels.ERROR, opts)
+    return
+  end
+  utils.cd_pioini()
+  local compile_commands_filename = 'compile_commands.json'
+  local compile_commands_path = '.pio' .. pathsep .. 'build' .. pathsep .. M.default_env .. pathsep .. compile_commands_filename
+  if not utils.file_exists(compile_commands_path) then
+    vim.notify(compile_commands_path .. ' does not exist to link it', vim.log.levels.ERROR, opts)
+    return
+  end
+  if utils.file_exists(compile_commands_filename) then
+    if not os.remove(compile_commands_filename) then
+      vim.notify(compile_commands_filename .. ' did not get removed', vim.log.levels.ERROR)
+      return
+    end
+  end
+  if config.clangd_source == 'compiledb' and utils.file_exists(compile_commands_path) then
+    if utils.is_windows then
+      os.execute('mklink ' .. compile_commands_filename .. ' ' .. compile_commands_path)
+    else
+      os.execute('ln -s ' .. compile_commands_path)
+    end
+  end
+end
 
 function M.pioenv()
   --- @type vim.log.levels
   local loglevel = vim.log.levels.INFO
-  if not utils.pio_install_check() then
-    return
-  end
   utils.cd_pioini()
 
+  local cmd
   -- TODO: execute target list command
-  local cmd = 'pio run --list-targets'
+  if utils.is_windows then
+    cmd = 'pio run --list-targets'
+  else
+    cmd = 'grep -F "[env:" < platformio.ini | sed "s/\\[env://" | sed "s/\\]//"'
+  end
   --- @type file*?
   local stdio_stream
   stdio_stream, _ = io.popen(cmd, 'r')
@@ -42,19 +77,25 @@ function M.pioenv()
   local envs = {}
   --- @type integer | nil
   local env_name_end
-  _ = stdio_stream.read(stdio_stream, '*line')
-  _ = stdio_stream.read(stdio_stream, '*line')
-  for line in stdio_stream.lines(stdio_stream, '*l') do -- readmode is for the output of lines as var `line`
-    env_name_end, _ = string.find(line, ' ', 2) -- if cmd succeeds, the env name has to be at least 1 char long and contains no spaces. skip the first one and search for space
-    env_name_end = env_name_end - 1
-    if env_name_end then -- would start at 1, regardless of start index, so we don't need to differentiate between 0 and nil
-      table.insert(envs, string.sub(line, 1, env_name_end))
-    else
-      vim.notify('formatting pio output failed on finding the first space char', vim.log.levels.ERROR)
-      io.close(stdio_stream)
-      return
-    end
+  if utils.is_windows then
     _ = stdio_stream.read(stdio_stream, '*line')
+    _ = stdio_stream.read(stdio_stream, '*line')
+  end
+  for line in stdio_stream.lines(stdio_stream, '*l') do -- readmode is for the output of lines as var `line`
+    if utils.is_windows then
+      env_name_end, _ = string.find(line, ' ', 2) -- if cmd succeeds, the env name has to be at least 1 char long and contains no spaces. skip the first one and search for space
+      env_name_end = env_name_end - 1
+      if env_name_end then -- would start at 1, regardless of start index, so we don't need to differentiate between 0 and nil
+        table.insert(envs, string.sub(line, 1, env_name_end))
+      else
+        vim.notify('formatting pio output failed on finding the first space char', vim.log.levels.ERROR)
+        io.close(stdio_stream)
+        return
+      end
+      _ = stdio_stream.read(stdio_stream, '*line')
+    else
+      table.insert(envs, line)
+    end
   end
 
   table.insert(envs, 'none')
@@ -62,6 +103,7 @@ function M.pioenv()
   -- TODO: execute picker
   picker.pick_env(envs, function(selected_env)
     M.default_env = selected_env
+    M.change_compiledb_symlink()
   end)
 end
 
